71e6fce92d46a1ac228e632b2a04a21da947a4ba
achainya <achainya@gmail.com>
2015-06-19 20:55:21 -0500

    Modificando setmode BCM a BOARD

--- blink.py
+++ blink.py
@@ -1,19 +1,19 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-#GPIO.setmode(GPIO.BOARD)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
 def blink():
 		print "Ejecucion iniciada..."
 		iteracion = 0
 		while iteracion < 30: ## Segundos que durara la funcion
-				GPIO.output(17, True) ## Enciendo el 17
-				GPIO.output(18, True) ## Enciendo el 18
+				GPIO.output(11, True) ## Enciendo el 17
+				GPIO.output(12, False) ## Enciendo el 18
 				time.sleep(1) ## Esperamos 1 segundo
-				GPIO.output(17, False) ## Apago el 17
-				GPIO.output(18, False) ## Apago el 18
+				GPIO.output(11, False) ## Apago el 17
+				GPIO.output(12, True) ## Apago el 18
 				time.sleep(1) ## Esperamos 1 segundo
 				iteracion = iteracion + 2 ## Sumo 2 porque he hecho dos parpadeos
 		print "Ejecucion finalizada"
